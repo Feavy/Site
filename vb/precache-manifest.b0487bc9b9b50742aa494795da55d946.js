f8caf34f1a663606d64c17e3977c3d1d92382882
Feavy <quentin071100@live.fr>
2019-03-13 22:36:10 +0100

    Ajout du Voltorbataille (qui marche)

---
+++ vb/precache-manifest.b0487bc9b9b50742aa494795da55d946.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "588f3cf357a1ac3b775f",
+    "url": "/vb/static/js/runtime~main.588f3cf3.js"
+  },
+  {
+    "revision": "40a593b3c7f522eca6e5",
+    "url": "/vb/static/js/main.40a593b3.chunk.js"
+  },
+  {
+    "revision": "19b1bc68ce5bb6c3f725",
+    "url": "/vb/static/js/1.19b1bc68.chunk.js"
+  },
+  {
+    "revision": "40a593b3c7f522eca6e5",
+    "url": "/vb/static/css/main.ad124475.chunk.css"
+  },
+  {
+    "revision": "58d330dc2c82ace8d5cc7807d7ea30a2",
+    "url": "/vb/index.html"
+  }
+];
--- vb/service-worker.js
+++ vb/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a3920b46e756c08639fdabcec3e144fd.js"
+  "/vb/precache-manifest.b0487bc9b9b50742aa494795da55d946.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/vb/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
