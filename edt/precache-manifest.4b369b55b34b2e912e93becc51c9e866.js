ad327df3503246085eb456fc454c3a8dfd92e33d
qgendar <quentin.gendarme@etu.univ-tlse2.fr>
2020-01-07 21:50:26 +0100

    Updated EDT : 4.0

---
+++ edt/precache-manifest.4b369b55b34b2e912e93becc51c9e866.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a62422e866964a7d8641",
+    "url": "/edt/static/js/runtime~main.a62422e8.js"
+  },
+  {
+    "revision": "d8fce27d1ff814eed6e6",
+    "url": "/edt/static/js/main.d8fce27d.chunk.js"
+  },
+  {
+    "revision": "3a91251c98619730f36f",
+    "url": "/edt/static/js/1.3a91251c.chunk.js"
+  },
+  {
+    "revision": "d8fce27d1ff814eed6e6",
+    "url": "/edt/static/css/main.a3d2c76e.chunk.css"
+  },
+  {
+    "revision": "d2b970945012b7e5c234dbc1489920a9",
+    "url": "/edt/index.html"
+  }
+];
--- edt/service-worker.js
+++ edt/service-worker.js
@@ -14,11 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-<<<<<<< HEAD
-  "/edt/precache-manifest.34a80337bda7bac3c23ba6f056e088ba.js"
-=======
-  "/edt/precache-manifest.9a4513225dbc3fa6ce3590ebc2166854.js"
->>>>>>> a185b7aca8aea904f4e971a91e1d888d8c36322f
+  "/edt/precache-manifest.4b369b55b34b2e912e93becc51c9e866.js"
 );
 
 workbox.clientsClaim();
@@ -34,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/edt/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
